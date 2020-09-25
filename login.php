6e07d8d73f7953294d112452b7f51f49138ba71e
Patrick <aschkan90@yahoo.de>
2020-09-25 11:12:30 +0200

    login function

--- login.php
+++ login.php
@@ -1,15 +1,31 @@
 <?php
 require('./lib/dbconfig.php');
+session_start();
 
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-$statement = $pdo->prepare("SELECT Username, Passwort FROM userverwaltung WHERE Username = ? AND Passwort = ?");
+    if(!empty($_POST['username'] && $_POST['password'])){
 
-$statement-> execute(array($username, $password));
-while($row = $statement->fetch()){
-    echo $row['Username'];
-    echo $row['Passwort'];
-}
+        $statement = $pdo->prepare("SELECT Username, Passwort FROM userverwaltung WHERE Username = ? AND Passwort = ?");
+
+        $statement-> execute(array($username, $password));
+        $row = $statement->fetch();
+
+        if ($username == $row['Username']) {
+
+            if (md5($password) == $row['Passwort']) {
+
+                $_SESSION['username'] = $row['Username'];
+                $_SESSION['loggedin'] = true;
+
+                header('Location: welcome.php');
+            }
+        }
+    }
+
+
+
+$pdo = null;
 
 ?>
